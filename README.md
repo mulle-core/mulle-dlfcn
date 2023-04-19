1230d19767f5f1968db44769ddf3a0cb7b39fba3
Nat! <nat@mulle-kybernetik.com>
2023-04-19 02:20:33 +0200

    squashed prerelease fixes

--- README.md
+++ README.md
@@ -9,9 +9,15 @@ looking up symbols with `dlysm`.
 
 On Windows this will use [dlfcn-win32]() to supply `dlsym` and relatives.
 
-### You are here
 
-![Overview](overview.dot.svg)
+| Release Version                                       | Release Notes
+|-------------------------------------------------------|--------------
+| ![Mulle kybernetiK tag](https://img.shields.io/github/tag/mulle-core/mulle-dlfcn.svg?branch=release) [![Build Status](https://github.com/mulle-core/mulle-dlfcn/workflows/CI/badge.svg?branch=release)](//github.com/mulle-core/mulle-dlfcn/actions)| [RELEASENOTES](RELEASENOTES.md) |
+
+
+
+
+
 
 
 ## Add
@@ -19,15 +25,23 @@ On Windows this will use [dlfcn-win32]() to supply `dlsym` and relatives.
 Use [mulle-sde](//github.com/mulle-sde) to add mulle-dlfcn to your project:
 
 ``` sh
-mulle-sde dependency add --c \
-                         --github mulle-core \
-                         --marks no-actual-link,no-singlephase \
-                         mulle-dlfcn
+mulle-sde add github:mulle-core/mulle-dlfcn
 ```
+
+To only add the sources of mulle-dlfcn with dependency
+sources use [clib](https://github.com/clibs/clib):
+
+
+``` sh
+clib install --out src/mulle-core mulle-core/mulle-dlfcn
+```
+
+Add `-isystem src/mulle-core` to your `CFLAGS` and compile all the sources that were downloaded with your project.
+
 
 ## Install
 
-### mulle-sde
+### Install with mulle-sde
 
 Use [mulle-sde](//github.com/mulle-sde) to build and install mulle-dlfcn and all dependencies:
 
@@ -38,7 +52,14 @@ mulle-sde install --prefix /usr/local \
 
 ### Manual Installation
 
-Install into `/usr/local`:
+Install the requirements:
+
+| Requirements                                 | Description
+|----------------------------------------------|-----------------------
+| [mulle-c11](https://github.com/mulle-c/mulle-c11)             | 🔀 Cross-platform C compiler glue (and some cpp conveniences)
+| [dlfcn-win32](https://github.com/mulle-core/dlfcn-win32)             | ===========
+
+Install **mulle-dlfcn** into `/usr/local` with [cmake](https://cmake.org):
 
 ``` sh
 cmake -B build \
@@ -49,5 +70,8 @@ cmake --build build --config Release &&
 cmake --install build --config Release
 ```
 
+## Author
+
+[Nat!](https://mulle-kybernetik.com/weblog) for Mulle kybernetiK
 
 
--- test/.mulle/etc/env/environment-host-ci-prerelease.sh
+++ test/.mulle/etc/env/environment-host-ci-prerelease.sh
@@ -1,37 +1,19 @@
 #
-# mulle-testallocator nodetype: ${MULLE__TESTALLOCATOR_NODETYPE:-tar}
+# mulle-dlfcn nodetype: ${MULLE__DLFCN_NODETYPE:-tar}
 #
-export MULLE__TESTALLOCATOR_NODETYPE="git"
+export MULLE__DLFCN_NODETYPE="git"
 
 
 #
-# mulle-testallocator url: ${MULLE__TESTALLOCATOR_URL:-https://github.com/mulle-core/mulle-testallocator/archive/${MULLE_TAG}.tar.gz}
+# mulle-dlfcn url: ${MULLE__DLFCN_URL:-https://github.com/mulle-core/mulle-dlfcn/archive/${MULLE_TAG}.tar.gz}
 #
-export MULLE__TESTALLOCATOR_URL="https://github.com/mulle-core/mulle-testallocator.git"
+export MULLE__DLFCN_URL="https://github.com/mulle-core/mulle-dlfcn.git"
 
 
 #
-# mulle-testallocator branch: ${MULLE__TESTALLOCATOR_BRANCH}
+# mulle-dlfcn branch: ${MULLE__DLFCN_BRANCH}
 #
-export MULLE__TESTALLOCATOR_BRANCH="prerelease"
-
-
-#
-# mulle-thread nodetype: ${MULLE__THREAD_NODETYPE:-tar}
-#
-export MULLE__THREAD_NODETYPE="git"
-
-
-#
-# mulle-thread url: ${MULLE__THREAD_URL:-https://github.com/mulle-concurrent/mulle-thread/archive/${MULLE_TAG}.tar.gz}
-#
-export MULLE__THREAD_URL="https://github.com/mulle-concurrent/mulle-thread.git"
-
-
-#
-# mulle-thread branch: ${MULLE__THREAD_BRANCH}
-#
-export MULLE__THREAD_BRANCH="prerelease"
+export MULLE__DLFCN_BRANCH="prerelease"
 
 
 #
@@ -53,60 +35,6 @@ export MULLE__C11_BRANCH="prerelease"
 
 
 #
-# mulle-allocator nodetype: ${MULLE__ALLOCATOR_NODETYPE:-tar}
-#
-export MULLE__ALLOCATOR_NODETYPE="git"
-
-
-#
-# mulle-allocator url: ${MULLE__ALLOCATOR_URL:-https://github.com/mulle-c/mulle-allocator/archive/${MULLE_TAG}.tar.gz}
-#
-export MULLE__ALLOCATOR_URL="https://github.com/mulle-c/mulle-allocator.git"
-
-
-#
-# mulle-allocator branch: ${MULLE__ALLOCATOR_BRANCH}
-#
-export MULLE__ALLOCATOR_BRANCH="prerelease"
-
-
-#
-# mulle-stacktrace nodetype: ${MULLE__STACKTRACE_NODETYPE:-tar}
-#
-export MULLE__STACKTRACE_NODETYPE="git"
-
-
-#
-# mulle-stacktrace url: ${MULLE__STACKTRACE_URL:-https://github.com/mulle-core/mulle-stacktrace/archive/${MULLE_TAG}.tar.gz}
-#
-export MULLE__STACKTRACE_URL="https://github.com/mulle-core/mulle-stacktrace.git"
-
-
-#
-# mulle-stacktrace branch: ${MULLE__STACKTRACE_BRANCH}
-#
-export MULLE__STACKTRACE_BRANCH="prerelease"
-
-
-#
-# mulle-dlfcn nodetype: ${MULLE__DLFCN_NODETYPE:-tar}
-#
-export MULLE__DLFCN_NODETYPE="git"
-
-
-#
-# mulle-dlfcn url: ${MULLE__DLFCN_URL:-https://github.com/mulle-core/mulle-dlfcn/archive/${MULLE_TAG}.tar.gz}
-#
-export MULLE__DLFCN_URL="https://github.com/mulle-core/mulle-dlfcn.git"
-
-
-#
-# mulle-dlfcn branch: ${MULLE__DLFCN_BRANCH}
-#
-export MULLE__DLFCN_BRANCH="prerelease"
-
-
-#
 # dlfcn-win32 nodetype: ${DLFCN__WIN32_NODETYPE:-tar}
 #
 export DLFCN__WIN32_NODETYPE="git"
@@ -122,41 +50,5 @@ export DLFCN__WIN32_URL="https://github.com/mulle-core/dlfcn-win32.git"
 # dlfcn-win32 branch: ${DLFCN__WIN32_BRANCH}
 #
 export DLFCN__WIN32_BRANCH="prerelease"
-
-
-#
-# mulle-atinit nodetype: ${MULLE__ATINIT_NODETYPE:-tar}
-#
-export MULLE__ATINIT_NODETYPE="git"
-
-
-#
-# mulle-atinit url: ${MULLE__ATINIT_URL:-https://github.com/mulle-core/mulle-atinit/archive/${MULLE_TAG}.tar.gz}
-#
-export MULLE__ATINIT_URL="https://github.com/mulle-core/mulle-atinit.git"
-
-
-#
-# mulle-atinit branch: ${MULLE__ATINIT_BRANCH}
-#
-export MULLE__ATINIT_BRANCH="prerelease"
-
-
-#
-# mulle-atexit nodetype: ${MULLE__ATEXIT_NODETYPE:-tar}
-#
-export MULLE__ATEXIT_NODETYPE="git"
-
-
-#
-# mulle-atexit url: ${MULLE__ATEXIT_URL:-https://github.com/mulle-core/mulle-atexit/archive/${MULLE_TAG}.tar.gz}
-#
-export MULLE__ATEXIT_URL="https://github.com/mulle-core/mulle-atexit.git"
-
-
-#
-# mulle-atexit branch: ${MULLE__ATEXIT_BRANCH}
-#
-export MULLE__ATEXIT_BRANCH="prerelease"
 
 
